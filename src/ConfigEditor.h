a248fb0691c9cc74a0090c0a6807c947db1120b7
fvla <frederick.v.l@gmail.com>
2016-10-24 19:46:50 -0400

    Cleaned up ConfigEditor code.

--- src/ConfigEditor.h
+++ src/ConfigEditor.h
@@ -1,33 +1,28 @@
-/*
- * ConfigEditor.h
- *
- *  Created on: Jan 23, 2016
- *      Author: ben
- */
-
 #ifndef SRC_CONFIGEDITOR_H_
 #define SRC_CONFIGEDITOR_H_
+
 #include "DriveStation.h"
 
-class ConfigEditor {
+class ConfigEditor
+{
 public:
-   ConfigEditor(DriveStation* DriveStation);
-   virtual ~ConfigEditor();
-   void update();
+    ConfigEditor(DriveStation* DriveStation);
+    virtual ~ConfigEditor();
+    void update();
 
-   void saveConfig();
-   void getConfig();
-   bool isType(std::string str, std::string type);
-   void showAllKeys();
+    void saveConfig();
+    void getConfig();
+    bool isType(std::string str, std::string type);
+    void showAllKeys();
 
-   int getInt(std::string key, int defaultValue = 0);
-   float getFloat(std::string key, float defaultValue = 0.0f);
-   double getDouble(std::string key, double defaultValue = 0.0);
-   std::string getString(std::string key, std::string defaultValue = "");
+    int getInt(std::string key, int defaultValue = 0);
+    float getFloat(std::string key, float defaultValue = 0.0f);
+    double getDouble(std::string key, double defaultValue = 0.0);
+    std::string getString(std::string key, std::string defaultValue = "");
 
-   void putProgDouble(std::string key, double value);
+    void putProgDouble(std::string key, double value);
 private:
-   DriveStation* m_DriveStation;
+    DriveStation* m_DriveStation;
 };
 
 #endif /* SRC_CONFIGEDITOR_H_ */
