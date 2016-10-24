10e0b4ed6603dfecbbd016cc75f737a3604e5fe0
fvla <frederick.v.l@gmail.com>
2016-10-24 19:32:53 -0400

    Added ConfigEditor to repository.

---
+++ src/ConfigEditor.h
@@ -0,0 +1,33 @@
+/*
+ * ConfigEditor.h
+ *
+ *  Created on: Jan 23, 2016
+ *      Author: ben
+ */
+
+#ifndef SRC_CONFIGEDITOR_H_
+#define SRC_CONFIGEDITOR_H_
+#include "DriveStation.h"
+
+class ConfigEditor {
+public:
+   ConfigEditor(DriveStation* DriveStation);
+   virtual ~ConfigEditor();
+   void update();
+
+   void saveConfig();
+   void getConfig();
+   bool isType(std::string str, std::string type);
+   void showAllKeys();
+
+   int getInt(std::string key, int defaultValue = 0);
+   float getFloat(std::string key, float defaultValue = 0.0f);
+   double getDouble(std::string key, double defaultValue = 0.0);
+   std::string getString(std::string key, std::string defaultValue = "");
+
+   void putProgDouble(std::string key, double value);
+private:
+   DriveStation* m_DriveStation;
+};
+
+#endif /* SRC_CONFIGEDITOR_H_ */
