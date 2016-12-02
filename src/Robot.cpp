9a93f97e65477ec6e0a79433298b84e3dc574f61
fvla <frederick.v.l@gmail.com>
2016-12-01 20:55:06 -0500

    Added Autonomous to Robot.cpp.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -7,6 +7,7 @@ class Robot : public SampleRobot
 {
     static const uint32_t LIMIT_SWITCH_PORT = 0;
 
+    AutoController m_auto;
     ConfigEditor m_config;
     LimitSwitch m_limitSwitch;
 
@@ -29,7 +30,17 @@ public:
             bool pressed = m_limitSwitch.get();
             SmartDashboard::PutString("DB/String 0", pressed ? "true" : "false");
 
-	    m_config.update();
+            m_config.update();
+
+            Wait(0.005); // wait 5ms to avoid hogging CPU cycles
+        }
+    }
+
+    void Autonomous()
+    {
+        while (IsAutonomous() && IsEnabled())
+        {
+            m_auto.run();
 
             Wait(0.005); // wait 5ms to avoid hogging CPU cycles
         }
